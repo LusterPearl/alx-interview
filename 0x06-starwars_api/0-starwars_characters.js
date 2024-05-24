8115992713ed695a9d5505cfecb76e8cef8b7761
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-24 05:33:42 -0700

    semi

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,23 +6,23 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
-    if (error) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
         console.error('Error:', error);
         return;
-    }
+      }
 
-    const film = JSON.parse(body);
-    const characters = film.characters;
-
-    characters.forEach(characterUrl => {
-        request(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error('Error:', error);
-                return;
-            }
-
-            const character = JSON.parse(body);
-            console.log(character.name);
-        });
+      const character = JSON.parse(body);
+      console.log(character.name);
     });
+  });
 });
