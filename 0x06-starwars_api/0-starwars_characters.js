de2c909a5b61f09d1b602eb13b63dba9a11aac7a
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-24 13:31:21 +0100

    tas

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,39 +2,27 @@
 // Import the request module
 const request = require('request');
 
-// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-// Define the API URL
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-// Make the API request
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  // Parse the response body as JSON
-  const film = JSON.parse(body);
-
-  // Get the list of characters
-  const characters = film.characters;
-
-  // Iterate over each character URL
-  characters.forEach(characterUrl => {
-    // Make a request to get each character's details
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
         return;
-      }
+    }
 
-      // Parse the character details
-      const character = JSON.parse(body);
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-      // Print the character's name
-      console.log(character.name);
+    characters.forEach(characterUrl => {
+        request(characterUrl, (error, response, body) => {
+            if (error) {
+                console.error('Error:', error);
+                return;
+            }
+
+            const character = JSON.parse(body);
+            console.log(character.name);
+        });
     });
-  });
 });
